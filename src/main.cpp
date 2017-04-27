b386ba9e4bf22c278c9cac4177bfd50ab46faace
Your Name <you@example.com>
2017-04-27 18:08:31 +0200

    un bonjour monsieur bonne vacance
    dit add.
    git

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 #include "mnist.h"
 #include<limits.h>
 
-
+ /** perceptron */
 	float linear_classifier(float* w , float* x){
 	float d=0;
 	for(int i=0;i<784;i++) {
-	d+=w[i]*x[i];
-	} 
-	if(d>0) return 1;
-	else return -1;
+		d+=w[i]*x[i];
+		} 
+		if(d>=0) return 1;
+		else return -1;
 	}
+
+	float dist_sq(float* a , float* b){
+	float d=0;
+	for(int i=0;i<784;i++) {
+		d+=(a[i]-b[i])*(a[i]-b[i]);
+		}
+	return d;
+	}
+
+float A[100][784];
+float B[100][784];
+const int K=10;
 
 
 
@@ -23,29 +35,84 @@
     //les 10000 images dont il faut deviner le label
  	float** test_images=read_mnist("t10k-images.idx3-ubyte");
 	float* test_labels=read_labels("t10k-labels.idx1-ubyte");
-	float*w=new float[784];
 
+
+
+//partie k-means
+
+int n[K];
+
+
+for(int i=0;i<K;i++) {
+	n[i]=0;
+	for(int j=0;j<784;j++) {
+		A[i][j]=images[i][j];
+		B[i][j]=0;
+	 }
+}
+
+
+for(int t=0;t<10;t++) {
+	printf("%u\n",t);
+	for(int i=0;i<K;i++) {
+		n[i]=0;
+		for(int j=0;j<784;j++) { 
+			B[i][j]=0; 
+		}
+	}
+
+	for(int i=0; i<60000; i++) {
+		printf("t= %u , i=%u\n",t,i);
+		float mind=-1; int gagnant=0;	
+		for(int k=0; k<K; k++) {
+			float d=dist_sq(A[k], images[i]);
+			if(d<=mind || mind==-1){
+				mind = d ;
+				gagnant = k;
+			}
+		}
+
+		for(int j=0; j<784; j++) B[gagnant][j]+= images[i][j];
+		n[gagnant]++;
+	}
+
+	for(int k=0;k <K; k++){
+		for(int j=0; j<784; j++) {
+			A[k][j] = B[k][j]/n[k];
+		}
+			save_jpg(A[k],28 ,28, "%u/%u.jpg",k,t);
+	}
+}
+
+
+//partie perceptron
+float*w=new float[784];
 
 //STEP 1: INITIALISATION 
 
-	for(int i=0;i<784;i++){w[i]=(float)rand()*2/INT_MAX-1;}
+	for(int i=0;i<784;i++){
+		w[i]=(float)rand()*2/INT_MAX-1;
+	}
 	float gamma = 0.01;
+
 
 //STEP 2 : LEARNING(que les donnes de train)
 
 	for(int i=0;i<60000;i++){
 		printf("%u\n",i);
+
 		//calcul de gradient (g=y*x si erreur , 0 sinon )
 		int prediction = linear_classifier(w,images[i]);
 		int verite = (labels[i] == 1) ? 1 : -1 ;
 		if(verite!=prediction){
 			printf("erreur\n");
 			// (w(t+1)=w(t)-gamma*y*x )
-				for(int j=0; j<784 ; j++){
-				w[j] = w[j] + gamma*verite*images[i][j];
-			}
+				for(int j=0; j<784 ; j++) {
+					w[j] = w[j] + gamma*verite*images[i][j];
+				}
 		}
 	}
+	
 
 //STEP 3 : TEST(que les donnes de test)
 
